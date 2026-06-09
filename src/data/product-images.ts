35f6f147a8cdd06bc733cd5e6e9fa4fe351c1d6b
be5498456 <65150119+be5498456@users.noreply.github.com>
2026-06-09 13:52:22 -0700

    Add curated product image URLs

--- src/data/product-images.ts
+++ src/data/product-images.ts
@@ -4,6 +4,96 @@
 // Do not use WordPress thumbnail URLs such as -200x200; use the original file URL for sharper images.
 
 export const productImageUrls: Record<string, string> = {
+  "mega-evolution-ascended-heroes-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/11/Ascended-Heroes-Elite-Trainer-Box-1.jpg",
+  "mega-evolution-ascended-heroes-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/11/Ascended-Heroes-Elite-Trainer-Box-Pokemon-Center.webp",
+  "mega-evolution-ascended-heroes-booster-bundle":
+    "https://www.pokebeach.com/news/2025/11/Pokemon_TCG_Mega_Evolution%E2%80%94Ascended_Heroes_Booster_Bundle-copy.jpg",
+
+  "mega-evolution-perfect-order-booster-pack":
+    "https://www.pokebeach.com/news/2026/01/Pokemon_TCG_Mega_Evolution%E2%80%94Perfect_Order_Booster_Wrap_Mega_Zygarde.jpg",
+  "mega-evolution-perfect-order-booster-display-box":
+    "https://www.pokebeach.com/news/2026/01/Perfect-Order-Booster-Box.jpg",
+  "mega-evolution-perfect-order-elite-trainer-box":
+    "https://www.pokebeach.com/news/2026/01/Pokemon_TCG_Mega_Evolution%E2%80%94Perfect_Order_Elite_Trainer_Box.jpg",
+  "mega-evolution-perfect-order-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2026/01/Perfect-Order-Pokemon-Center-Elite-Trainer-Box-Tyrunt.webp",
+  "mega-evolution-perfect-order-booster-bundle":
+    "https://www.pokebeach.com/news/2026/01/Perfect-Order-Booster-Bundle.jpg",
+
+  "mega-evolution-phantasmal-flames-booster-pack":
+    "https://www.pokebeach.com/news/2025/09/Pokemon_TCG_Mega_Evolution%E2%80%94Phantasmal_Flames_Booster_Wrap_Mega_Charizard.png",
+  "mega-evolution-phantasmal-flames-booster-display-box":
+    "https://www.pokebeach.com/news/2025/09/Phantasmal-Flames-Box.jpeg",
+  "mega-evolution-phantasmal-flames-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/09/Phantasmal-Flames-ETB.jpeg",
+  "mega-evolution-phantasmal-flames-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/09/Phantasmal-Flames-Pokemon-Center-ETB.webp",
+  "mega-evolution-phantasmal-flames-booster-bundle":
+    "https://www.pokebeach.com/news/2025/09/Phantasmal-Flames-Booster-Bundle-1.jpg",
+  "mega-evolution-phantasmal-flames-build-battle-box":
+    "https://www.pokebeach.com/news/2025/09/Phantasmal-Flames-Build-Battle.webp",
+  "mega-charizard-x-ex-ultra-premium-collection":
+    "https://www.pokebeach.com/news/2025/09/Mega-Charizard-X-ex-Ultra-Premium-Collection.jpg",
+
+  "mega-evolution-booster-pack":
+    "https://www.pokebeach.com/news/2025/07/Pokemon_TCG_Mega_Evolution_Booster_Wrap_Mega_Venusaur-copy.jpg",
+  "mega-evolution-booster-display-box":
+    "https://www.pokebeach.com/news/2025/09/Mega-Evolution-Box.webp",
+  "mega-evolution-booster-bundle":
+    "https://www.pokebeach.com/news/2025/07/Mega-Evolution-Booster-Bundle-copy.jpg",
+  "mega-evolution-build-battle-box":
+    "https://www.pokebeach.com/news/2025/07/Mega-Evolution-Build-Battle-2-copy.jpg",
+  "mega-lucario-ex-figure-collection":
+    "https://www.pokebeach.com/news/2025/08/20250818_184616.jpg",
+
+  "black-bolt-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/05/Black-Bolt-ETB.webp",
+  "black-bolt-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/05/Black-Bolt-ETB-PC.jpg",
+  "black-bolt-booster-bundle":
+    "https://www.pokebeach.com/news/2025/05/Black-Bolt-Booster-Bundle.webp",
+  "white-flare-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/05/White-Flare-ETB.webp",
+  "white-flare-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/05/White-Bolt-ETB-PC.jpg",
+  "white-flare-booster-bundle":
+    "https://www.pokebeach.com/news/2025/05/White-Flare-Booster-Bundle.webp",
+
+  "scarlet-violet-destined-rivals-booster-pack":
+    "https://www.pokebeach.com/news/2025/03/Pokemon_TCG_Scarlet_Violet%E2%80%94Destined_Rivals_Booster_Wraps_Cynthia_Garchomp.png",
+  "scarlet-violet-destined-rivals-booster-display-box":
+    "https://www.pokebeach.com/news/2025/03/Destined-Rivals-Booster-Box.webp",
+  "scarlet-violet-destined-rivals-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/03/Pokemon_TCG_Scarlet_Violet%E2%80%94Destined_Rivals_Elite_Trainer_Box.png",
+  "scarlet-violet-destined-rivals-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/03/Destined-Rivals-ETB-PC.jpg",
+  "scarlet-violet-destined-rivals-build-battle-box":
+    "https://www.pokebeach.com/news/2025/03/Destined-Rivals-Build-Battle.webp",
+  "scarlet-violet-destined-rivals-booster-bundle":
+    "https://www.pokebeach.com/news/2025/03/Destined-Rivals-Booster-Bundle.webp",
+
+  "scarlet-violet-journey-together-booster-pack":
+    "https://www.pokebeach.com/news/2025/01/Pokemon_TCG_Scarlet_Violet%E2%80%94Journey_Together_Booster_Wrap_Iono_s_Bellibolt.png",
+  "scarlet-violet-journey-together-booster-display-box":
+    "https://www.pokebeach.com/news/2025/01/Journey-Together-Booster-Box-Closed.jpg",
+  "scarlet-violet-journey-together-elite-trainer-box":
+    "https://www.pokebeach.com/news/2025/01/Journey-Together-ETB-Front-N.jpg",
+  "scarlet-violet-journey-together-booster-bundle":
+    "https://www.pokebeach.com/news/2025/01/Journey-Together-Booster-Bundle.jpg",
+  "journey-together-lillie-premium-tournament-collection":
+    "https://www.pokebeach.com/news/2025/02/Lillie-Premium-Tournament-Collection-1.webp",
+
+  "scarlet-violet-surging-sparks-booster-pack":
+    "https://www.pokebeach.com/news/2024/08/Pokemon_TCG_Scarlet_Violet%E2%80%94Surging_Sparks_Booster_Wrap_Pikachu.png",
+  "scarlet-violet-surging-sparks-booster-display-box":
+    "https://www.pokebeach.com/news/2024/08/Pokemon-TCG-Scarlet-Violet-Surging-Sparks-Booster-Display_EN-copy-1.jpg",
+  "scarlet-violet-surging-sparks-elite-trainer-box":
+    "https://www.pokebeach.com/news/2024/08/Pokemon_TCG_Scarlet_Violet%E2%80%94Surging_Sparks_Elite_Trainer_Box.png",
+  "scarlet-violet-surging-sparks-pokemon-center-elite-trainer-box":
+    "https://www.pokebeach.com/news/2024/08/Pokemon_TCG_Scarlet_Violet%E2%80%94Surging_Sparks_Pokemon_Center_Elite_Trainer_Box.png",
+
   "prismatic-evolutions-pokemon-center-elite-trainer-box":
     "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Pokemon_Center_Elite_Trainer_Box-1.jpg",
   "prismatic-evolutions-elite-trainer-box":
