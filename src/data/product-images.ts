8684b638de1c71151747c18648eb9ce784f83de9
be5498456 <65150119+be5498456@users.noreply.github.com>
2026-06-09 12:01:29 -0700

    Use full-size product image URLs

--- src/data/product-images.ts
+++ src/data/product-images.ts
@@ -1,39 +1,40 @@
 // Product image URLs are manually curated from public product-lineup articles or official retailer pages.
 // Prefer official Pokemon Center / Pokemon product-gallery image URLs when available.
-// PokeBeach is used for product-lineup images when official pages are blocked or unavailable.
+// PokeBeach is used for product-lineup images when official image URLs are blocked or unavailable.
+// Do not use WordPress thumbnail URLs such as -200x200; use the original file URL for sharper images.
 
 export const productImageUrls: Record<string, string> = {
   "prismatic-evolutions-pokemon-center-elite-trainer-box":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Pokemon_Center_Elite_Trainer_Box-1-200x191.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Pokemon_Center_Elite_Trainer_Box-1.jpg",
   "prismatic-evolutions-elite-trainer-box":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Elite_Trainer_Box-1-200x191.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Elite_Trainer_Box-1.jpg",
   "prismatic-evolutions-binder-collection":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Binder_Collection-200x200.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Binder_Collection.jpg",
   "prismatic-evolutions-poster-collection":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Poster_Collection-200x200.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Poster_Collection.jpg",
   "prismatic-evolutions-mini-tin":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Mini_Tin-200x200.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Mini_Tin.jpg",
   "prismatic-evolutions-surprise-box":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Surprise_Box-200x200.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Surprise_Box.jpg",
   "prismatic-evolutions-booster-bundle":
-    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Booster_Bundle-200x200.jpg",
+    "https://www.pokebeach.com/news/2024/11/Pokemon_TCG_Scarlet_Violet%E2%80%94Prismatic_Evolutions_Booster_Bundle.jpg",
 
   "scarlet-violet-151-pokemon-center-elite-trainer-box":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Pokemon_Center_Elite_Trainer_Box-200x191.png",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Pokemon_Center_Elite_Trainer_Box.png",
   "scarlet-violet-151-elite-trainer-box":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Elite_Trainer_Box-200x191.png",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Elite_Trainer_Box.png",
   "scarlet-violet-151-poster-collection":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Poster_Collection-180x200.jpg",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Poster_Collection.jpg",
   "scarlet-violet-151-binder-collection":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Binder_Collection-146x200.jpg",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Binder_Collection.jpg",
   "scarlet-violet-151-booster-bundle":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Booster_Bundle-149x200.png",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Booster_Bundle.png",
   "scarlet-violet-151-ultra-premium-collection":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Ultra-Premium_Collection-200x141.png",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_Ultra-Premium_Collection.png",
   "scarlet-violet-151-alakazam-ex-collection":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_ex_Box%E2%80%94Alakazam_ex-160x200.jpg",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_ex_Box%E2%80%94Alakazam_ex.jpg",
   "scarlet-violet-151-zapdos-ex-collection":
-    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_ex_Box%E2%80%94Zapdos_ex-200x181.jpg",
+    "https://www.pokebeach.com/news/2023/06/Pokemon_TCG_Scarlet_Violet%E2%80%94151_ex_Box%E2%80%94Zapdos_ex.jpg",
   "scarlet-violet-151-mini-tin":
-    "https://www.pokebeach.com/news/2023/06/Pokemon-TCG-Scarlet-Violet-151-Mini-Tin-Display-Front_EN-3376x4386-43f1fe7-154x200.jpg",
+    "https://www.pokebeach.com/news/2023/06/Pokemon-TCG-Scarlet-Violet-151-Mini-Tin-Display-Front_EN-3376x4386-43f1fe7.jpg",
 };
